ca853582d9f27818c8cd062d6c56c90cb4e7e96c
Zecoder <70232378+EzekielJRamirez@users.noreply.github.com>
2022-03-22 22:23:36 -0700

    coded first page

--- src/main.java
+++ src/main.java
@@ -1,2 +1,38 @@
+import java.util.Vector;
+
 public class main {
+    // 170 - 175
+    // 170
+    Vector<Vector<Vector<Integer>>> userView = perspectiveFromRectangle(cameraView, base);
+    Graphics g = appFrame.getGraphics();
+    Graphics2D g2D = (Graphics2D) g;
+
+    for (int = 0; i < rectHeight; i++) {
+        for (int j = 0; j < rectWidth; j++) {
+            int alpha = 255;
+            int red = userView.elementAt(i).elementAt(j).elementAt(0);
+            int green = userView.elementAt(i).elementAt(j).elementAt(1);
+            int blue = userView.elementAt(i).elementAt(j).elementAt(2);
+
+            while (red < 0) {
+                red = red + 256;
+            }
+            while (256 <= red) {
+                red = red - 256;
+            }
+            while (green < 0) {
+                green = green + 256;
+            }
+            while (256 <= green) {
+                green = green - 256;
+            }
+            while (blue < 0) {
+                blue = blue + 256;
+            }
+            while (256 <= blue) {
+                blue = blue - 256;
+            }
+            // page 171
+        }
+    }
 }
